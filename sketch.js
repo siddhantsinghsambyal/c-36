d45b53d696184751646dde59cfd60dac0be3f56a
siddhantsinghsambyal <76777192+siddhantsinghsambyal@users.noreply.github.com>
2021-04-18 16:59:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas;
+var database;
+var gameState=0;
+var playerCount;
+var form, player, game;
+function setup(){
+    canvas= createCanvas(400,400);
+    database=firebase.database();
+
+game=new Game();
+game.getState();
+game.start();
+  
+}
+
+function draw(){
+
+}
